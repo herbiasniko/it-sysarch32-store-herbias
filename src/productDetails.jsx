157e7d4a7d341c1602d592eaa9a93626da1e2421
Niko Herbias <78730-22256176@SERVER2.DOMAIN>
2024-05-11 12:22:29 +0800

    first

--- src/productDetails.jsx
+++ src/productDetails.jsx
@@ -2,10 +2,14 @@ import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import { db } from './config/firebase.js';
 import { doc, getDoc } from 'firebase/firestore';
+import { loadStripe } from '@stripe/stripe-js';
+
+const stripePromise = loadStripe('pk_test_51PF6IoFftbvOfYAcSAuVRTrncOmz3HrUzDjAleRMNaee78uJFP97msW3AIuI1aMPwhopK28WjetHbPBHxXIhLSTX00tANCw2XO'); 
 
 function productDetails() {
   const { id } = useParams();
   const [product, setProduct] = useState(null);
+
 
   useEffect(() => {
     async function fetchProduct() {
@@ -19,7 +23,34 @@ function productDetails() {
     }
     fetchProduct();
   }, [id]);
+  const handleClick = async (product_name, product_price) => {
+    const stripe = await stripePromise;
 
+    // Send a request to the backend to create a checkout session
+    const response = await fetch('http://localhost:4001/create-checkout-session', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ product_name, product_price }), // Send product name and price to the backend
+    });
+
+    if (response.ok) {
+      // If the request is successful, retrieve the session ID from the response
+      const session = await response.json();
+
+      // Redirect the user to the Stripe Checkout page using the session ID
+      const result = await stripe.redirectToCheckout({ sessionId: session.id });
+
+      if (result.error) {
+        // If there is an error during the redirect, display the error message
+        setError(result.error.message);
+      }
+    } else {
+      // If there is an error creating the checkout session, display an error message
+      setError('Error creating checkout session');
+    }
+  };
   if (!product) {
     return <div>Loading...</div>;
   }
@@ -34,6 +65,7 @@ function productDetails() {
 
         <p>Description: {product.product_description}</p>
         <p>Quantity: ${product.product_quantity}</p>
+        <button onClick={()=>handleClick(product.product_name,product.product_price, product.product_price*100)}>Checkout</button>
     
       </div>
     </div>
